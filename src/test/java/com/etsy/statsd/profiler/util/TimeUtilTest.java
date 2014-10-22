37da375af1ffc62c43958e21dc3b7333b48afc83
Andrew Johnson <ajohnson@etsy.com>
2014-10-21 21:43:48 -0400

    Additional test case

--- src/test/java/com/etsy/statsd/profiler/util/TimeUtilTest.java
+++ src/test/java/com/etsy/statsd/profiler/util/TimeUtilTest.java
@@ -12,6 +12,7 @@ public class TimeUtilTest {
         assertEquals(1, TimeUtil.convertReportingPeriod(1, TimeUnit.MILLISECONDS, 100, TimeUnit.MICROSECONDS));
         assertEquals(10000, TimeUtil.convertReportingPeriod(1, TimeUnit.MILLISECONDS, 10, TimeUnit.SECONDS));
         assertEquals(1000, TimeUtil.convertReportingPeriod(10, TimeUnit.MILLISECONDS, 10, TimeUnit.SECONDS));
+        assertEquals(100000, TimeUtil.convertReportingPeriod(100, TimeUnit.MICROSECONDS, 10, TimeUnit.SECONDS));
     }
 
     @Test
